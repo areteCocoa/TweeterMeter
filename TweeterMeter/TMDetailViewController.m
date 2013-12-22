0125b1bb9a384e473a22cd7878f38a261c8f201c
thomas-ring <thomasjring@me.com>
2013-12-22 09:23:34 -0800

    Added limited Twitter support

--- TweeterMeter/TMDetailViewController.m
+++ TweeterMeter/TMDetailViewController.m
@@ -37,7 +37,84 @@
 
     if (self.detailItem) {
         self.navigationItem.title = [[self.detailItem valueForKey:@"term"] description];
-        self.detailDescriptionLabel.text = [[self.detailItem valueForKey:@"term"] description];
+        
+        __block NSString *detailString;
+        
+        ACAccountStore *store = [[ACAccountStore alloc] init];
+        if ([SLComposeViewController
+             isAvailableForServiceType:SLServiceTypeTwitter]) {
+            
+            //  Step 1:  Obtain access to the user's Twitter accounts
+            ACAccountType *twitterAccountType =
+            [store accountTypeWithAccountTypeIdentifier:
+             ACAccountTypeIdentifierTwitter];
+            
+            [store
+             requestAccessToAccountsWithType:twitterAccountType
+             options:NULL
+             completion:^(BOOL granted, NSError *error) {
+                 if (granted) {
+                     //  Step 2:  Create a request
+                     NSArray *twitterAccounts =
+                     [store accountsWithAccountType:twitterAccountType];
+                     NSURL *url = [NSURL URLWithString:@"https://api.twitter.com"
+                                   @"/1.1/statuses/user_timeline.json"];
+                     NSDictionary *params = @{@"screen_name" : @"thomasjring",
+                                              @"include_rts" : @"0",
+                                              @"trim_user" : @"1",
+                                              @"count" : @"1"};
+                     SLRequest *request =
+                     [SLRequest requestForServiceType:SLServiceTypeTwitter
+                                        requestMethod:SLRequestMethodGET
+                                                  URL:url
+                                           parameters:params];
+                     
+                     //  Attach an account to the request
+                     [request setAccount:[twitterAccounts lastObject]];
+                     
+                     //  Step 3:  Execute the request
+                     [request performRequestWithHandler:
+                      ^(NSData *responseData,
+                        NSHTTPURLResponse *urlResponse,
+                        NSError *error) {
+                          
+                          if (responseData) {
+                              if (urlResponse.statusCode >= 200 &&
+                                  urlResponse.statusCode < 300) {
+                                  
+                                  NSError *jsonError;
+                                  NSDictionary *timelineData =
+                                  [NSJSONSerialization
+                                   JSONObjectWithData:responseData
+                                   options:NSJSONReadingAllowFragments error:&jsonError];
+                                  if (timelineData) {
+                                      detailString = [[NSString stringWithFormat:@"%@", timelineData] description];
+                                      NSLog(@"Timeline Response: %@\n", detailString);
+                                      dispatch_async(dispatch_get_main_queue(), ^{
+                                          self.detailDescriptionLabel.text = detailString;
+                                      });
+                                  }
+                                  else {
+                                      // Our JSON deserialization went awry
+                                      NSLog(@"JSON Error: %@", [jsonError localizedDescription]);
+                                  }
+                              }
+                              else {
+                                  // The server did not respond ... were we rate-limited?
+                                  NSLog(@"The response status code is %d",
+                                        urlResponse.statusCode);
+                              }
+                          }
+                      }];
+                 }
+                 else {
+                     // Access was not granted, or an error occurred
+                     NSLog(@"%@", [error localizedDescription]);
+                 }
+             }];
+        }
+        
+        self.detailDescriptionLabel.text = detailString;
     }
 }
 
@@ -45,6 +122,8 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    // Update modal with newer tweets
+    
     [self configureView];
 }
 
